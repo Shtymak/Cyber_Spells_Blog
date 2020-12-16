34455c947ed47bae58a207278466deeac192a656
Shtymak <57435107+Shtymak@users.noreply.github.com>
2020-12-16 13:52:53 +0200

    Editable

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,16 @@ class PostsController < ApplicationController
     def new
   
     end
+
+    def update
+        @post =Post.find(params[:id])
+        @post.update(post_params)
+    redirect_to @post
+    end
+
+    def edit
+        @post =Post.find(params[:id])
+    end
   
     def create
       @post = Post.new(post_params)
