74d9fa4a90d70a02ac8761c94a658ebb43e1cdab
Shtymak <57435107+Shtymak@users.noreply.github.com>
2020-12-16 03:42:38 +0200

    Add pages "All posts"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     def index
+        @post = Post.all
     end
   
     def show
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
     end
   
     def create
-      @post = Post.create(post_params)
+      @post = Post.new(post_params)
       @post.save
       redirect_to @post
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  root to:'pages#index',as:'home'
+  get 'posts/new',as:'post_new'
+  
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
